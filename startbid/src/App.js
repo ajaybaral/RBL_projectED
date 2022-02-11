042096cab957f3deb964abe8f1ea9cdfc652993c
suriyaa333 <tsuriyaa@gmail.com>
2022-02-11 19:59:40 +0530

    init 3

--- startbid/src/App.js
+++ startbid/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='ml-auto'>hi</h1>
+
+    <h1 className='ml-3'>hiiiii</h1>
+
+
     </div>
   );
 }
