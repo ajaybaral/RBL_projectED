5c6f263dd9ed42202ca8eeb10d48758032e4932f
Bharath Babu <66842073+bharathbabu68@users.noreply.github.com>
2022-02-12 23:03:46 +0530

    Update auctions.js

--- startbid/src/Resources/auctions.js
+++ startbid/src/Resources/auctions.js
@@ -5,47 +5,47 @@ export const auctions = [
         "title":"Antico Orologio",
         "price":10,
         "description": "A unique vintage pocket watch from 1678",
-        "category":"werable"
+        "ending_date": "03-04-2022"
     },
     {
         "id":2,
         "link":"https://romanovrussia.com/wp-content/uploads/wwDSC05355.jpg",
         "title":"Victorian Five",
         "price":112,
-        "description": "A unique vintage pocket watch from 1678",
-        "category":"werable"
-    },
-    {
-        "id":4,
-        "link":"https://cpimg.tistatic.com/05472639/b/4/Aladdin-Lamp-in-Brass-Gennie-Lamp.jpg",
-        "title":"Aladin's Lamp",
-        "price":10,
-        "description": "A unique vintage pocket watch from 1678",
-        "category":"werable"
-    },
-    {
-        "id":6,
-        "link":"https://cpimg.tistatic.com/05472639/b/4/Aladdin-Lamp-in-Brass-Gennie-Lamp.jpg",
-        "title":"Aladin's Lamp",
-        "price":10,
-        "description": "A unique vintage pocket watch from 1678",
-        "category":"werable"
+        "description": "A unique piece of vintage furniture from 18th century",
+        "ending_date": "02-04-2022"
     },
     {
         "id":3,
-        "link":"https://previews.123rf.com/images/cookelma/cookelma1409/cookelma140900082/31600267-vintage-antico-orologio-da-tasca-.jpg",
-        "title":"Antico Orologio",
+        "link":"https://cpimg.tistatic.com/05472639/b/4/Aladdin-Lamp-in-Brass-Gennie-Lamp.jpg",
+        "title":"Aladin's Lamp",
         "price":10,
-        "description": "A unique vintage pocket watch from 1678",
-        "category":"werable"
+        "description": "One of the most famous lamps in the world",
+        "ending_date": "01-05-2022"
     },
     {
-        "id":5,
-        "link":"https://romanovrussia.com/wp-content/uploads/wwDSC05355.jpg",
-        "title":"Victorian Five",
-        "price":112,
-        "description": "A unique vintage pocket watch from 1678",
-        "category":"werable"
+        "id": 4,
+        "link": 'https://www.thesprucecrafts.com/thmb/AC7AtusOTVbnpAbnOFtVAmLpGLI=/434x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/WorldRecordChineseBowl-5a78b8d1a18d9e0036a9c277.jpg',
+        "title": 'Ru Guanyao Brush Washer Bowl',
+        "price": 52,
+        "description": '"Such Ru guanyao wares known for their intense blue-green glaze and “ice-crackle” pattern–are extremely rare because the kiln in China’s central Henan province',
+        "ending_date": '14-05-2022'
     },
-    
+    {
+        "id": 5,
+        "link": 'https://www.thesprucecrafts.com/thmb/dnPSWuMl_ZkCxqHHCi3pCHkDDYA=/434x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/henry-graves-watch-5a83732fc5542e00377fbe48.jpg',
+        "title": 'Patek Philippe Supercomplication Pocket Watch',
+        "price": 75,
+        "description": '" Known as the Henry Graves Jr. Supercomplication, it is named after "a New York banker who competed with the car manufacturer James Ward Packard to commission the most elaborate watch possible in the early 20th century,',
+        "ending_date": '10-10-2022',
+    },
+    {
+        "id": 6,
+        "link": 'https://freepikpsd.com/file/2019/10/antique-items-png-Transparent-Images.png',
+        "title": "Antique hand",
+        "price": 10,
+        "description": "A unique antique hand from the 16th century",
+        "ending_date": "01-05-2022"
+    }
+
 ]
